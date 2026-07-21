09d3ee72f2d617315a99ad9592f766325c12529e
bernhardhelmer-admin <bernhard.helmer@outlook.com>
2026-07-21 10:22:27 +0200

    First commit, versioning files

---
+++ src/test/java/io/everyonecodes/project_module/ProjectModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.everyonecodes.project_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
