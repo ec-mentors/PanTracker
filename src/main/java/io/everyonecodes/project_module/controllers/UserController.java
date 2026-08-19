883d7dcc5817d924e396ecbc38138850e3372364
bernhardhelmer-admin <bernhard.helmer@outlook.com>
2026-08-19 21:37:13 +0200

    created UserController

--- src/main/java/io/everyonecodes/project_module/controllers/UserController.java
+++ src/main/java/io/everyonecodes/project_module/controllers/UserController.java
@@ -1,7 +1,32 @@
 package io.everyonecodes.project_module.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import io.everyonecodes.project_module.dtos.requests.UserRegisterRequest;
+import io.everyonecodes.project_module.dtos.responses.UserResponse;
+import io.everyonecodes.project_module.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    ResponseEntity<UserResponse> registerUser(@Valid @RequestBody UserRegisterRequest request) {
+        UserResponse response = userService.registerUser(request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/login")
+    ResponseEntity<UserResponse> loginUser (@RequestParam String username) {
+        UserResponse response = userService.loginUser(username);
+
+        return ResponseEntity.ok(response);
+    }
 }
